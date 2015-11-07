24dce8e629082ecadad94e867394c929010626e4
Matt <mattmartell07@gmail.com>
2015-11-07 16:16:09 -0500

    Adding Proper Solution Structure

---
+++ MCollarApi2/Controllers/FencesController.cs
@@ -0,0 +1,15 @@
+﻿using MCollarApi2.BusinessLayer;
+using MCollarApi2.Model;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace MCollarApi2.Controllers
+{
+    [EnableCors(origins: "http://mcollarapi2.azurewebsites.net", headers: "*", methods: "*")]
+    public class FencesController : ApiController
+    {
+
+    }
+}
---
+++ MCollarApi2/Model/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MCollarApi2.Model
+{
+    public class CollarLocation
+    {
+        public int CollarId { get; set; }
+        
+        public int LocationId { get; set; }
+        public DateTime RecordTime { get; set; }
+        public double XCoord { get; set; }
+        public double YCoord { get; set; }
+    }
+}
